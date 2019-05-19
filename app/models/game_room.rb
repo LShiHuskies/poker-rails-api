0111ae91e5bb13e5a79c94ff3d1d52bb087bdfa4
Louis Shi <Shilou2010@gmail.com>
2019-05-18 20:20:06 -0400

    game_room.rb

--- app/models/game_room.rb
+++ app/models/game_room.rb
@@ -1,2 +1,8 @@
 class GameRoom < ApplicationRecord
+  has_many :messages
+  has_many :users, through: :user_game_rooms
+
+  validates :name, presence: true, uniqueness: true
+  validates :smallblind, presence: true
+  validates :bigblind, presence: true
 end
