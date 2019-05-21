af614a0fb41ff30b345c0c7a03d15b39906f6734
Louis Shi <Shilou2010@gmail.com>
2019-05-21 01:27:54 -0400

    game_rooms_controller

---
+++ app/controllers/api/game_rooms_controller.rb
@@ -0,0 +1,64 @@
+class Api::GameRoomsController < ApplicationController
+  before_action :requires_login, only: [:index]
+
+  def index
+    # token = request.headers["Authorization"]
+
+    # authenticate(Game.all)
+
+    # if !valid_token?
+    #   render json: {
+    #     message: 'INFO ENTERED IS WRONG!!!'
+    #   }, status: :unauthorized
+    # else
+    #   render json: Game.all
+    # end
+    #
+    # requires_login()
+    render json: GameRoom.all
+  end
+
+  def create
+    @game_room = GameRoom.new
+    @game_room.name = params[:name]
+    @game_room.smallblind = params[:smallblind]
+    @game_room.bigblind = params[:bigblind]
+
+    if @game_room.save
+      ActionCable.server.broadcast 'GameRoomsChannel', {
+        id: @game_room.id
+        name: @game_room.name
+        smallblind: @game_room.smallblind
+        bigblind: @game_room.bigblind
+      }
+
+      render json: {
+        id: @game_room.id
+        name: @game_room.name
+        smallblind: @game_room.smallblind
+        bigblind: @game_room.bigblind
+      }
+
+    else
+      render json: {
+        errors: @game_room.errors.full_messages
+      }, status: :unprocessable_entity
+    end
+
+  end
+
+
+  def show
+    @game_room = GameRoom.find_by(id: params[:id])
+
+    render json: @game_room
+  end
+
+  def update
+    @game_room = GameRoom.find_by(id: params[:id])
+
+  end
+
+
+
+end
